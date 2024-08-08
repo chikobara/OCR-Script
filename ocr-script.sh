de723c605958ac00a08bd5aa46935e0689548bed
Chikobara <albaraa.as.2002@gmail.com>
2024-08-08 17:51:55 +0300

    japanese empty space

--- ocr-script.sh
+++ ocr-script.sh
@@ -9,6 +9,10 @@ gnome-screenshot -a -f $TMPDIR/screenshot.png
 # Process the screenshot with Tesseract and save the result to a text file in the temporary directory
 tesseract -l eng+jpn+ara $TMPDIR/screenshot.png $TMPDIR/output 
 
+# Post-process the output to remove unnecessary spaces between Japanese characters
+sed -i 's/\([一-龥ぁ-ゔァ-ヴー々〆〤]\) \+/\1/g' $TMPDIR/output.txt
+sed -i 's/\([ぁ-ゔァ-ヴー々〆〤]\) \([ぁ-ゔァ-ヴー々〆〤]\)/\1\2/g' $TMPDIR/output.txt
+
 # Copy the result to the clipboard, preserving Unicode characters
 cat $TMPDIR/output.txt | xclip -selection clipboard -in -target UTF8_STRING
 
@@ -16,4 +20,3 @@ echo "Text copied to clipboard. Temporary file: $TMPDIR/output.txt"
 
 # Optionally, remove the temporary directory when done
 rm -r $TMPDIR
-
